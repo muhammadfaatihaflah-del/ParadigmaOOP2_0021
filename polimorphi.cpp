7c2a2ae20a8a3636e51f2adf26871f190e6be048
muhammadfaatihaflah-del <muhammadfaatihaflah@gmail.com>
2026-05-18 10:13:57 +0700

    membuat kerangka anak class joko

--- polimorphi.cpp
+++ polimorphi.cpp
@@ -4,4 +4,11 @@ using namespace std;
 class seseorang {
     public :
         virtual void pesan()= 0;      
+        // virtual void pesan(){
+        //      cout <<"pesan dari seseorang"<<endl;
+        //}
+};
+
+class joko :public seseorang{
+
 };
