e88b0649c9dcd4b24eb08dba8145f279010850dc
muhammadfaatihaflah-del <muhammadfaatihaflah@gmail.com>
2026-05-18 10:46:56 +0700

    membuat kerangka program utama

--- abstraksi.cpp
+++ abstraksi.cpp
@@ -13,6 +13,10 @@ class abstraksiklas{
         //menampilkan nilai
         void display(){
             cout << "x = " << x <<endl;
-            cout << "y = " << x <<endl;
+            cout << "y = " << y <<endl;
         }
 };
+
+int main(){
+    
+}
